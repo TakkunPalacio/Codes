839ab9e784b2386432feb73e303b0c8fa10f1310
TakkunPalacio <paltakkun176@gmail.com>
2023-11-23 09:48:22 +0800

    Update Lab6.m

--- matlab/Lab6.m
+++ matlab/Lab6.m
@@ -4,15 +4,28 @@ subplot(3,1,1);
 stem(n,x);
 xlabel('n');
 ylabel("y");
-title("a. 2*impseq(-2,-5,5) - impseq(4,-5,5)");
+title("a.");
 
 n = 0:20
-x = n * (stepseq(0,0,20) - stepseq(10,0,20)) + 10*exp(-0.3*(n-10))*(stepseq(10,0,20) - stepseq(20,0,20));
+a = stepseq(0,0,20) - stepseq(10,0,20);
+b = 10*exp(-0.3*(n-10));
+c= stepseq(10,0,20) - stepseq(20,0,20);
+x = n.*(a)+b.*c;
 subplot(3,1,2);
 stem(n,x);
 xlabel('n');
 ylabel("y");
-title("b. n ");
+title("b. ");
+
+%c. x(n) = cos(0.04*pi*n) + 0.2*randn(size(n)), n = 0,1,2,...,50;
+n = 0:50;
+x = cos(0.04*pi*n) + 0.2*randn(size(n));
+subplot(3,1,3);
+stem(n,x);
+xlabel('n');
+ylabel("y");
+title("c. ");
+
 
 function [x,n] = impseq(n0,n1,n2)
     n = [n1:n2]; x = [(n-n0) == 0];
