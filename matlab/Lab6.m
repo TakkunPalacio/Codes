268f1d764053abcb50cc7ba7b871811c9c11323e
TakkunPalacio <paltakkun176@gmail.com>
2023-11-23 09:40:38 +0800

    Update Lab6.m

--- matlab/Lab6.m
+++ matlab/Lab6.m
@@ -7,7 +7,7 @@ ylabel("y");
 title("a. 2*impseq(-2,-5,5) - impseq(4,-5,5)");
 
 n = 0:20
-x = n .* (stepseq(0,0,20) - stepseq(10,0,20)) + 10*exp(-0.3*(n-10))*(stepseq(10,0,20) - stepseq(20,0,20));
+x = n * (stepseq(0,0,20) - stepseq(10,0,20)) + 10*exp(-0.3*(n-10))*(stepseq(10,0,20) - stepseq(20,0,20));
 subplot(3,1,2);
 stem(n,x);
 xlabel('n');
